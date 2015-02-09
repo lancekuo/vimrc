76254e7665b57c09f087dcd79b1822cdf5b41aaf
Lance Kuo (IS-TW) <lance_kuo@trend.com.tw>
2015-02-09 17:49:19 +0800

    Add shell script for installing from curl command.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+VIMHOME=~/.vim
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+[ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
+[ -e "~/.vim" ] && die "~/.vim already exists."
+[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+
+git clone ssh://git@gitlab.trendnet.org:222/environment/vim.git "$VIMHOME"
+cd "$VIMHOME"
+git submodule update --init
+
+cd ~
+ln -s .vim/vimrc .vimrc
+
+echo "Your VIM configuration has been installed."
