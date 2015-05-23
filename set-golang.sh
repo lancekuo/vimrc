f3892f20f5b4210b0079a801b89e757215a53107
Lance Kuo (RD-TW) <lance_kuo@trend.com.tw>
2015-05-23 21:57:01 +0800

    Add support for MAC OS, darwin on installing golang relative tools.

--- set-golang.sh
+++ set-golang.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-echo "Please enter your GOPATH: [default: /vagrant]"
+if [ `go env GOOS` == "darwin" ]; then
+    defaultgopath="$HOME/Projects"
+else
+    defaultgopath="/vagrant"
+fi;
+echo "Please enter your GOPATH: [default: $defaultgopath]"
 read gopath
 if [ "$gopath" == "" ]; then
-    gopath="/vagrant";
+    gopath=$defaultgopath;
 fi;
-echo -e "export GOPATH=$gopath\nexport PATH=$PATH:\$GOPATH/bin" > /etc/profile.d/golang.sh
-source /etc/profile
+
+if [ `go env GOOS` == "darwin" ]; then
+    echo -e "export GOPATH=$gopath\nexport PATH=$PATH:\$GOPATH/bin" >> ~/.bash_profile
+    source ~/.bash_profile
+else
+    echo -e "export GOPATH=$gopath\nexport PATH=$PATH:\$GOPATH/bin" > /etc/profile.d/golang.sh
+    source /etc/profile
+fi;
 
 go get -u github.com/jstemmer/gotags
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
-apt-get install -y ctags
+if [ `go env GOOS` == "darwin" ]; then
+    brew install -y ctags
+else
+    apt-get install -y ctags
+fi;
