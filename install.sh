1b5a258094b24f673a660fdcdf7bf07dfcde5fe4
Lance Kuo <lancekuo@gmail.com>
2017-04-12 00:24:33 -0400

    Append bash prompt, docker-machine prompt and docker-compose prompt to .bash_profile.

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ cd ~
 ln -s .vim/vimrc .vimrc
 ln -s .vim/tmux.conf .tmux.conf
 
+echo 'source ~/.vim/prompt.bash/docker-machine-prompt.bash' > ~/.bash_profile
+echo 'source ~/.vim/prompt.bash/docker-compose-prompt.bash' >> ~/.bash_profile
 echo 'source ~/.vim/prompt.bash/bash-prompt.bash' >> ~/.bash_profile
 
 echo "Your VIM configuration has been installed."
